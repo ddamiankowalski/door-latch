88d49d2417bffe3484997a91e1950d581b2cb504
Damian Kowalski <dkowalski.1997@gmail.com>
2022-08-19 03:05:44 +0200

    zmiany

--- src/app/fixed-menu/fixed-menu.component.ts
+++ src/app/fixed-menu/fixed-menu.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fixed-menu',
   templateUrl: './fixed-menu.component.html',
-  styleUrls: ['./fixed-menu.component.css']
+  styleUrls: ['./fixed-menu.component.css'],
+  animations: [
+    trigger('slide', [
+      state('true', style({
+        opacity: 0,
+        visibility: 'hidden'
+      })),
+      state('false', style({
+        opacity: 1,
+        visibility: 'visible'
+      })),
+      transition('true <=> false', [
+        animate('.3s')
+      ])
+    ])
+  ]
 })
 export class FixedMenuComponent implements OnInit {
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if(window.scrollY > 250) {
+      this.isHidden = false;
+    } else {
+      this.isHidden = true;
+    }
+  }
+
+  public isHidden: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  menuList: any = [
+    {name: 'Home'},
+    {name: 'Contact'},
+    {name: 'Our Work'},
+    {name: 'Pricing'}
+  ]
 
 }
